5ea66909aa45a9abc4fb2057b21976789167b1c3
anasty223 <86348493+anasty223@users.noreply.github.com>
2022-09-19 20:51:31 +0300

    First commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,45 +1,41 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
 console.log(JSON.stringify(galleryItems));
 console.log(galleryItems);
-const imageContainer = document.querySelector('.gallery');
+const imageContainer = document.querySelector(".gallery");
 const cardsMarkup = createImagesCard(galleryItems);
-imageContainer.insertAdjacentHTML('beforeend', cardsMarkup)
-imageContainer.addEventListener('click', onContainerClick)
-
+imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+imageContainer.addEventListener("click", onContainerClick);
 
 function createImagesCard(galleryItems) {
-
-    return galleryItems.map(({ preview, original }) => {
-
-        return `
+  return galleryItems
+    .map(({ preview, original }) => {
+      return `
      <a class="gallery__item" href="${original}">
     <img class="gallery__image" src="${preview}" alt="Image description" />
     </a>
-   `
-    }).join('');
-
-};
+   `;
+    })
+    .join("");
+}
 
 function onContainerClick(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    const isGalleryImage = evt.target.classList.contains('gallery__image')
-    if (!isGalleryImage) {
-        return;
-    }
-   
+  const isGalleryImage = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImage) {
+    return;
+  }
 }
 
 const lightbox = new SimpleLightbox(".gallery a", {
   captionSelector: "img",
   captionsData: "alt",
-  captionPosition: "bottom", 
-  captionDelay: 250, 
- });
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 console.log(lightbox);
 // =======================================================================
